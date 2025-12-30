1d4b417940813e027810fe37dcf59ec9316408b0
mclxxiv <a.isakov1@g.nsu.ru>
2025-12-30 18:10:44 +0700

    task_6

--- homework/tasks/task_6.py
+++ homework/tasks/task_6.py
@@ -15,18 +15,18 @@ class BackgroundCoroutinesWatcher:
         self._running_tasks: Set[asyncio.Task] = set()
 
     def schedule_soon(self, coro: Coroutine):
-        pass
         # Здесь необходимо реализовать логику планирования корутины.
-        #
-        # YOUR CODE GOES HERE
+        task = asyncio.create_task(coro)
+        self._running_tasks.add(task)
 
     def _remove_from_running_task(self, task: asyncio.Task) -> None:
         self._running_tasks.remove(task)
 
     async def close(self):
+        _, pending = await asyncio.wait(self._running_tasks, timeout=1)
+        for task in pending:
+            task.cancel()
         # Здесь необходимо реализовать отмену корутин, которые ещё не успели завершиться.
-        #
-        # YOUR CODE GOES HERE
 
 
 class FastHandlerWithLongBackgroundTask:
